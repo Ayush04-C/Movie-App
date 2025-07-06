15b90c70069454af7553dabb43e5222cd9a82c77
ayush <workside@gamil.com>
2025-07-06 23:20:36 +0530

    Initial

---
+++ src/components/spinner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './spinner.css'; // Assuming you have a spinner.css file for styling
+
+const Spinner = () => {
+  return (
+    <span className="loader"></span>
+  );
+}
+
+export default Spinner;
